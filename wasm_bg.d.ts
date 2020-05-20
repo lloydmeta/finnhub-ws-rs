7d182b12b8b67f1d94a5f80a5c6732691f7b8df3
lloydmeta <lloydmeta@users.noreply.github.com>
2020-05-20 05:29:44 +0000

    deploy: f361d71cffedde28fa8730cb0f32f4037fc8dbfd

--- wasm_bg.d.ts
+++ wasm_bg.d.ts
@@ -5,7 +5,7 @@ export function run_app(): void;
 export function __wbindgen_malloc(a: number): number;
 export function __wbindgen_realloc(a: number, b: number, c: number): number;
 export const __wbindgen_export_2: WebAssembly.Table;
-export function _dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1f84d2308df0a38b(a: number, b: number, c: number): void;
 export function _dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h0d5fe26f110d6079(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h1f84d2308df0a38b(a: number, b: number, c: number): void;
 export function __wbindgen_exn_store(a: number): void;
 export function __wbindgen_start(): void;
